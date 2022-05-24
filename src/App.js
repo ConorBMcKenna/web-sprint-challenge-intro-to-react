980cc026e54c857ace433070da5ff80949e1777a
Conor McKenna <conormckenna63@yahoo.com>
2022-05-23 18:36:05 -0600

    Completed MVP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import Character from "./components/Character";
 
 const App = () => {
+
+  const [characters, setCharacters] = useState([
+    {
+    name: "Brian",
+    status: "Alive"
+    },
+    {
+      name: "Johnny number",
+      status: "Alive"
+    },
+    {
+        name: "Conor",
+        status: "Alive"
+    },
+
+  ])
+  console.log("It Rendered!")
+
+  useEffect(()=>{
+    console.log("this runs after the first render")
+    axios.get("https://swapi.dev/api/people/")
+    .then((res) => {
+      console.log(res.data)
+      setCharacters(res.data)
+    })
+  }, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,8 +40,16 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map((character, index)=>(
+          <Character 
+          key = {`App-Charachters-Map-${character.name}-${index}`}
+          character={character}
+          />
+        ))
+      }   
     </div>
   );
-}
+} 
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import react from 'react'
+
+const Character = (props) => {
+    console.log(props)
+
+    const character = props.character
+    
+    return (
+        <article >
+            <p>{character.name} is {character.status}</p>
+
+        </article>
+    )
+}
+
+export default Character
